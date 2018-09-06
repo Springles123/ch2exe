af24821ceb93776a238255431c6a82f53b4f5702
kyra.jacox <kyra.jacox@CLC-LAB209-15.TCSD.tooelesd.org>
2018-09-06 14:05:03 -0600

    ch2exe test

---
+++ chapter 2 exe/kyrajch2exe/CHAPTER_2_EXAMPLE.java
@@ -0,0 +1,15 @@
+package kyrajch2exe;
+/*
+ * Kyra Jacox
+ * 9-6-18
+ * ch2 exe
+ */
+public class CHAPTER_2_EXAMPLE {
+
+	public static void main(String[] args) {
+		// display some text
+		System.out.println("Golden Flowers");
+		
+	}
+
+}
